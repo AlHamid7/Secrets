ab145a20bf8b29263f89a673373d8c39edcc7a8f
AlHamid <alhamidiledcad@gmail.com>
2021-01-28 16:11:11 +0530

    port listen env process

--- app.js
+++ app.js
@@ -173,6 +173,11 @@ req.login(user, function(err){
 });
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
